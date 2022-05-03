86ef4062e4c5beea7d92f8b6c1ef83e4c73f9bf2
Kyle Shepard <kshepard379@gmail.com>
2022-05-02 21:10:19 -0700

    created game board with custom board size

--- bullship.js
+++ bullship.js
@@ -2,22 +2,47 @@
 var gameWindow = document.getElementById("gameWindow");
 var board;
 
-newGame(8, 8);
+newGame(8, "standard");
 
-function newGame(width, height, gameType){
+function newGame(size, gameType){
     // clear board
     // reset gamewindow
     gameWindow.innerHTML = "";
     // reset ships and game progress
 
-    if(width > 0 && height > 0){
-        // TODO: place ships along the grid, or don't if doing impossible mode
+    if(size > 0){
 
-        // create the gui
-        for(var row = 0; row < height; row++){
-            for(var col = 0; col < width; col++){
+        // calculate tile and gap size
+        // hardcoded gap size does not scale well, use algebra to get this equation, where gap to tile width ratio is 1:10
+        tileSize = 8000 / (11 * size + 1); // original equation: tileSize = (800 - ((size + 1) * gapSize)) / size;
+        gapSize = tileSize / 10;
+        
+        // create the grid
+        for(var rowVal = 0; rowVal < size; rowVal++){
 
+            //calculate y offset
+            dY = gapSize + (rowVal * (tileSize + gapSize));
+
+            for(var colVal = 0; colVal < size; colVal++){
+
+                //calculate x offset
+                dX = gapSize + (colVal * (tileSize + gapSize));
+
+                //add tile to DOM
+                var tile = document.createElement("div");
+                gameWindow.appendChild(tile);
+
+                //initialize tile and place on game board
+                tile.id = "tile-" + ((rowVal * size) + colVal);
+                tile.className = "tile";
+                tile.style.top = Math.round(dY) + "px";
+                tile.style.left = Math.round(dX) + "px";
+                tile.style.width = Math.round(tileSize) + "px";
             }
         }
+    }
+
+    if (gameType != "bullship"){
+        // TO DO: place ships
     }
 }

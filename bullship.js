6536c6cf55df2e7dc6e5f4021b9d888589576d64
Kyle Shepard <kshepard379@gmail.com>
2022-05-11 22:58:20 -0700

    added hardcoded ship placement and hit detection

--- bullship.js
+++ bullship.js
@@ -1,19 +1,36 @@
-
+//html objects
 var gameWindow = document.getElementById("gameWindow");
 var reset = document.getElementById("reset");
+
+//game state
 var board;
 var currentGameMode;
+var shotsRemaining;
+var hitsRemaining;
 
-newGame(8, "classic");
+//start new game on classic mode 8*8
+newGame(8, 0);
 
+/**
+ * 
+ * @param {int} size - (8 <= n <= 24) sets the n*n the board size 
+ * @param {int} gameMode (0 <= n <= 1) 0 = classic, 1 = bullship
+ */
 function newGame(size, gameMode){
     
-    if(size <= 0) return
+    //bound size between 8 and 24
+    size = Math.min(Math.max(8, size), 24);
 
-    // clear board
+    //default to classic if gamemode is invalid
+    gameMode = (gameMode < 0 || gameMode > 1) ? 0 : gameMode;
+
     // reset gamewindow
     gameWindow.innerHTML = "";
+
     // reset ships and game progress
+    board = [];
+    shotsRemaining = 24; //hardcoded until basic game logic finished
+    hitsRemaining = 9; // ^ ditto ^
 
     // calculate tile and gap size
     // hardcoded gap size does not scale well, use algebra to get this equation, where gap to tile width ratio is 1:10
@@ -46,16 +63,28 @@ function newGame(size, gameMode){
         }
     }
 
-    if (gameMode != "bullship"){
-        // TODO: place ships
+    if (gameMode == 0){ //if classic mode
+        // TODO: place ships programmatically
 
+        //right now, we are hardcoding 3 ships of sizes 2, 3, and 4 like the Windwaker battleship minigame
+        // 0 = water, 1 = ship, 2 = miss, 3 = hit
+        board = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0]
+        ];
     }
 
-    currentGameMode = gameMode;
+    currentGameMode = gameMode ?? "classic";
 }
 
 reset.addEventListener("click", function(){
-    newGame(8, "classic")
+    newGame(8, 0)
 })
 
 gameWindow.addEventListener("click", function(e){
@@ -65,12 +94,22 @@ gameWindow.addEventListener("click", function(e){
         var xPos = e.target.getAttribute("xPos");
         var yPos = e.target.getAttribute("yPos");
 
-        if(currentGameMode == "bullship"){
-
+        if(currentGameMode == 1){
+            alert("bullship mode not implemented yet!")
         } else {
-            e.target.style.background = "red";
+            var tileHit = board[yPos][xPos];
+            switch (tileHit){
+                case 0:
+                    e.target.style.background = "white";
+                    board[yPos][xPos] = 2;
+                    break;
+                case 1:
+                    e.target.style.background = "red";
+                    board[yPos][xPos] = 3;
+                    break;
+                default:
+                    alert("bruh")
+            }
         }
-
-        // alert(xPos + ", " + yPos);
     }
 });
